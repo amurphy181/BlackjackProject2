a50dfcd1a93d57412f62a1bd86e7cf505ab77d80
Alex Murphy <alexmurphy@Andrews-MacBook-Pro-2.local>
2020-02-14 14:37:15 -0700

    initial commit

---
+++ src/com/skilldistillery/blackjack/cards/Deck.java
@@ -0,0 +1,32 @@
+package com.skilldistillery.blackjack.cards;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+	
+	private List<Card> cards;
+	
+	public Deck() {
+		cards = new ArrayList<Card>();
+		for (Suit suit : Suit.values()) {
+			for (Rank rank : Rank.values()) {
+				cards.add(new Card(rank, suit));
+			}
+		}
+	}
+	
+	public int checkDeckSize() {
+		return cards.size();
+	}
+	
+	public Card dealCard() {
+		return cards.remove(0);
+	}
+	
+	public void shuffle() {
+		Collections.shuffle(cards);
+	}
+
+}
---
+++ src/com/skilldistillery/blackjack/cards/Suit.java
@@ -0,0 +1,20 @@
+package com.skilldistillery.blackjack.cards;
+
+public enum Suit {
+	HEARTS("Hearts"), 
+	SPADES("Spades"), 
+	CLUBS("Clubs"), 
+	DIAMONDS("Diamonds");
+	
+	Suit(String name) {
+		this.name = name;
+	}
+	
+	private String name;
+	
+	@Override
+	public String toString() {
+		return name;
+	}
+	
+}
